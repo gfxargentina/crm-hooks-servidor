87b0d7d647d354fde811fe3d6cafdd6dd960d287
Luis Chilo <gfxargentina@gmail.com>
2020-07-07 21:38:19 -0300

    se modifico para utilizar mongodb atlas

--- config/db.js
+++ config/db.js
@@ -1,12 +1,29 @@
 const mongoose = require ('mongoose');
+require('dotenv').config({ path: 'variables.env' });
+
+// const conectarDB = async () => {
+//     try {
+//         await mongoose.connect('mongodb://localhost:27017/curso', {useNewUrlParser: true, useUnifiedTopology: true});
+//         console.log('DB Conectada');    
+//     } catch (error) {
+//         console.log('Hubo un error');
+//         console.log(error);
+//     }
+// }
 
 const conectarDB = async () => {
     try {
-        await mongoose.connect('mongodb://localhost:27017/curso', {useNewUrlParser: true, useUnifiedTopology: true});
-        console.log('DB Conectada');    
+        await mongoose.connect(process.env.DB_MONGO, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+            useCreateIndex: true
+        });
+        console.log('DB Conectada');
     } catch (error) {
         console.log('Hubo un error');
         console.log(error);
+        process.exit(1); // detener la app
     }
 }
 
